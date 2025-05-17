13fdd0a0b0d77d0930cea80bd8970a792158a9a1
Enes Karaoğlan <eneskaraoglan8@outlook.com>
2025-05-17 16:32:40 +0300

    Firebase entegresini yaptım.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,15 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4") // Burada çift tırnak ve parantez zorunlu
+        classpath("com.google.gms:google-services:4.3.15") // Aynı şekilde
+
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -12,6 +24,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
@@ -19,3 +32,6 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+apply(plugin = "com.google.gms.google-services")
+
