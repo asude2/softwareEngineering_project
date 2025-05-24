7e311c53e01e79b916637219e6012f901dddd3a0
asude <asuderuken63@gmail.com>
2025-05-24 20:43:31 +0300

    uygulamanın nasıl kullanıldığına dair sayfaya yönlendirecek butonunu ekledim

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// Menü sayfası (drawer) - 2 buton var
 class MenuSayfasi extends StatelessWidget {
   const MenuSayfasi({super.key});
 
@@ -35,6 +36,32 @@ class MenuSayfasi extends StatelessWidget {
               ],
             ),
           ),
+          // Nasıl Kullanılır butonu
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFFDC321E),
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NasilKullanilir()),
+                );
+              },
+              child: Text(
+                'Nasıl Kullanılır',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+
+          // Çök - Kapan - Tutun butonu
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: ElevatedButton(
@@ -64,6 +91,70 @@ class MenuSayfasi extends StatelessWidget {
   }
 }
 
+// Nasıl Kullanılır sayfası
+class NasilKullanilir extends StatelessWidget {
+  const NasilKullanilir({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Nasıl Kullanılır?"),
+        foregroundColor: Colors.white,
+        backgroundColor: Color(0xFFDC321E),
+      ),
+      body: Container(
+        color: Colors.white,
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Uygulamayı Kullanma Rehberi",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "1. İhbar oluşturabilmek için kayıt olup sayfanıza giriş yapmalısınız.\n"
+                  "2. İhbar haritasını ve listesini görmek için uygulamaya kayıt olmanıza gerek yoktur.\n"
+                  "3. Yetkiliyseniz, kurtarılan kişilerin konumlarını ve durumlarını güncelleyebilirsiniz.\n"
+                  "4. Haritada kırmızı ile gösterilen konumlar onaylanmış ihbarları, yeşil ile gösterilen konumlar kurtarılan kişileri göstermektedir.\n"
+                  "5. Güncellenen konumlar herkes tarafından görülebilir.",
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 30),
+            Image.asset('assets/usageInfo.png'),
+            SizedBox(height: 50),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 12),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.arrow_back, color: Colors.white, size: 30),
+                    SizedBox(width: 20),
+                    Text(
+                      'Ana Sayfaya Dön',
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Çök - Kapan - Tutun sayfası
 class CokKapanTutunSayfasi extends StatelessWidget {
   const CokKapanTutunSayfasi({super.key});
 
@@ -72,6 +163,7 @@ class CokKapanTutunSayfasi extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Çök-Kapan-Tutun'),
+        backgroundColor: Color(0xFFDC321E),
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -103,8 +195,8 @@ class CokKapanTutunSayfasi extends StatelessWidget {
               SizedBox(height: 24),
               Image.asset(
                 'assets/çkt.jpg',
-                height: 400,
-                width: 800,
+                width: double.infinity,
+                fit: BoxFit.contain,
               ),
             ],
           ),
